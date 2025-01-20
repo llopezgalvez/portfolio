bc06061a63f495d02845381457a0aa3a55cb23e6
Lester López <lopezgalvezlester@gmail.com>
2025-01-20 17:22:06 -0600

    the navbar has been updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Navbar.css';
 import { Code2, FolderCode, Home, Mail, User2 } from 'lucide-react';
 import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
 
@@ -11,39 +12,39 @@ export const Navbar = () => {
         <nav className="navbar navbar-expand-lg bg-transparent">
             <div className="container-fluid">
                 <h4 className="name-nav me-auto">
-                    <a href="/">
-                        <Code2 size={40}/>
-                    </a>
+                    <Link to="/">
+                        <Code2 size={40} />
+                    </Link>
                 </h4>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
+                    <ul className="navbar-nav ms-auto gap-1">
                         <li className="nav-item">
-                            <a className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} href="/">
+                            <Link className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} to="/">
                                 <Home className="nav-icon" size={20} /> Inicio
-                            </a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`} href="/about">
+                            <Link className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`} to="/about">
                                 <User2 className="nav-icon" size={20} /> Sobre mí
-                            </a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a className={`nav-link ${location.pathname === '/skills' ? 'active' : ''}`} href="/skills">
+                            <Link className={`nav-link ${location.pathname === '/skills' ? 'active' : ''}`} to="/skills">
                                 <Code2 className="nav-icon" size={20} /> Habilidades
-                            </a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a className={`nav-link ${location.pathname === '/projects' ? 'active' : ''}`} href="/projects">
+                            <Link className={`nav-link ${location.pathname === '/projects' ? 'active' : ''}`} to="/projects">
                                 <FolderCode className="nav-icon" size={20} /> Proyectos
-                            </a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a className={`nav-link ${location.pathname === '/contact' ? 'active' : ''}`} href="/contact">
+                            <Link className={`nav-link ${location.pathname === '/contact' ? 'active' : ''}`} to="/contact">
                                 <Mail className="nav-icon" size={20} /> Contacto
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
